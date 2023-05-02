448eb7934d6c7e6eb9b4ab105d1c9f8bf333175b
RalPan <raluca1pantilie@gmail.com>
2023-05-02 17:20:51 +0100

    Add project files.

---
+++ MVC-SIS_UI/Controllers/StudentController.cs
@@ -0,0 +1,123 @@
+﻿using MVC_SIS_Data;
+using MVC_SIS_Models;
+using MVC_SIS_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_SIS_UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var model = StudentRepository.GetAll();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            
+            var viewModel = new AddStudentVM();
+            viewModel.SetCourseItems(CourseRepository.GetAll());
+            viewModel.SetMajorItems(MajorRepository.GetAll());
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(AddStudentVM studentVM)
+        {
+            
+             if (!ModelState.IsValid)
+            {
+                studentVM.SetCourseItems(CourseRepository.GetAll());
+                studentVM.SetMajorItems(MajorRepository.GetAll());
+                return View(studentVM);
+            }
+             
+            studentVM.Student.Courses = new List<Course>();
+
+            foreach (var id in studentVM.SelectedCourseIds)
+                studentVM.Student.Courses.Add(CourseRepository.Get(id));
+
+            studentVM.Student.Major = MajorRepository.Get(studentVM.Student.Major.MajorId);
+
+            StudentRepository.Add(studentVM.Student);
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult EditStudent(int id)
+        {
+            EditStudentVM viewmodel = new EditStudentVM();
+            viewmodel.SetCourseItems(CourseRepository.GetAll());
+            viewmodel.SetMajorItems(MajorRepository.GetAll());
+            viewmodel.SetStateItems(StateRepository.GetAll());
+            viewmodel.Student = StudentRepository.Get(id);
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult EditStudent(EditStudentVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.SetCourseItems(CourseRepository.GetAll());
+                viewModel.SetMajorItems(MajorRepository.GetAll());
+                viewModel.SetStateItems(StateRepository.GetAll());
+                viewModel.Student = StudentRepository.Get(viewModel.Student.StudentId);
+                return View(viewModel);
+            }
+            
+            viewModel.Student.Major = MajorRepository.Get(viewModel.Student.Major.MajorId);
+            foreach(int id in viewModel.SelectedCourseIds)
+            {
+                if (!viewModel.Student.Courses.Contains(CourseRepository.Get(id)))
+                {
+                    viewModel.Student.Courses.Add(CourseRepository.Get(id));
+                }
+                
+            }
+
+            if(viewModel.Student.Address != null)
+            {
+                if (viewModel.Student.Address.State.StateAbbreviation != null)
+                {
+                    viewModel.Student.Address.State = StateRepository.Get(viewModel.Student.Address.State.StateAbbreviation);
+                }
+                    
+            }
+            StudentRepository.Edit(viewModel.Student);
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteStudent(int id)
+        {
+            DeleteStudentVM viewModel = new DeleteStudentVM();
+            viewModel.currentStudent = StudentRepository.Get(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteStudent(DeleteStudentVM viewModel)
+        {
+            StudentRepository.Delete(viewModel.currentStudent.StudentId);
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ MVC-SIS_UI/Models/AddStudentVM.cs
@@ -0,0 +1,100 @@
+﻿using MVC_SIS_Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_SIS_UI.Models
+{
+    public class AddStudentVM : IValidatableObject
+    {
+        public Student Student { get; set; }
+        public List<SelectListItem> CourseItems { get; set; }
+        public List<SelectListItem> MajorItems { get; set; }
+        public List<SelectListItem> StateItems { get; set; }
+        public List<int> SelectedCourseIds { get; set; }
+
+        public AddStudentVM()
+        {
+            CourseItems = new List<SelectListItem>();
+            MajorItems = new List<SelectListItem>();
+            StateItems = new List<SelectListItem>();
+            SelectedCourseIds = new List<int>();
+            Student = new Student();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (Student == null || Student.FirstName == "" || Student.FirstName == null)
+            {
+                errors.Add(new ValidationResult("Please enter the student's first name.",
+                    new[] { "Student.FirstName"}));
+            }
+
+            if (Student == null || Student.LastName == "" || Student.LastName == null)
+            {
+                errors.Add(new ValidationResult("Please enter the student's last name.",
+                    new[] { "Student.LastName" }));
+            }
+
+            if (Student == null || string.IsNullOrEmpty(Student.GPA.ToString()))
+            {
+                errors.Add(new ValidationResult("Please enter the student's GPA.",
+                    new[] { "Student.GPA" }));
+            }
+
+            if (Student == null || string.IsNullOrEmpty(Student.Major.MajorId.ToString()))
+            {
+                errors.Add(new ValidationResult("Please select the student's major.",
+                    new[] { "Student.Major.MajorId" }));
+            }
+
+            if (SelectedCourseIds.Count == 0)
+            {
+                errors.Add(new ValidationResult("Please select the student's courses.",
+                    new[] { "Student.Courses" }));
+            }
+
+            return errors;
+        }
+
+        public void SetCourseItems(IEnumerable<Course> courses)
+        {
+            foreach (var course in courses)
+            {
+                CourseItems.Add(new SelectListItem()
+                {
+                    Value = course.CourseId.ToString(),
+                    Text = course.CourseName
+                });
+            }
+        }
+
+        public void SetMajorItems(IEnumerable<Major> majors)
+        {
+            foreach (var major in majors)
+            {
+                MajorItems.Add(new SelectListItem()
+                {
+                    Value = major.MajorId.ToString(),
+                    Text = major.MajorName
+                });
+            }
+        }
+
+        public void SetStateItems(IEnumerable<State> states)
+        {
+            foreach (var state in states)
+            {
+                StateItems.Add(new SelectListItem()
+                {
+                    Value = state.StateAbbreviation,
+                    Text = state.StateName
+                });
+            }
+        }
+    }
+}
